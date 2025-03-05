59af1af76a596bd366996c5bd2dfba710047b8fa
rahulpandey-create <rahul6395595358@gmail.com>
2025-03-05 15:07:20 +0000

    basics completed

---
+++ basics/strings.js
@@ -0,0 +1,5 @@
+let str1 = "Rahul"
+let str2 = "pandey"
+console.log (str1+str2)
+let str = "Something" 
+console.log(str)
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,4 @@
 console.log("Hello");
+// primitive data type me (stack) memory use hoti hai
+// heap non - primitive me use hoti hai
+let myYoutubename = "Rahul pandey"
